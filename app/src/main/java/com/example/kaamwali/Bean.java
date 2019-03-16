090f15cb3d6bf788c55142735f0cc8e61a0fa920
nisha <nisharasiya13@gmail.com>
2019-03-16 16:36:52 +0530

    dsfdsfds

--- app/src/main/java/com/example/kaamwali/Bean.java
+++ app/src/main/java/com/example/kaamwali/Bean.java
@@ -1,10 +1,33 @@
 package com.example.kaamwali;
 
 import android.app.Application;
+import android.content.Context;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
 public class Bean extends Application {
 
+    private static Context context;
 
     public  String BaseUrl = "http://ec2-13-126-246-74.ap-south-1.compute.amazonaws.com/";
+
     public String userid = "userid";
+
+
+    public static Context getContext() {
+        return context;
+    }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        context = getApplicationContext();
+
+        FontsOverride.setDefaultFont(this  , "MONOSPACE" , "calibri.ttf");
+
+
+        ImageLoader.getInstance().init(ImageLoaderConfiguration.createDefault(this));
+
+    }
 }
